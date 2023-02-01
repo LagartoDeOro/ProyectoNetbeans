c484b6e58947383e5be6e4dd535fc79d65f3933f
LagartoDeOro <122525607+LagartoDeOro@users.noreply.github.com>
2023-02-01 12:51:19 +0100

    Update ProyectoNetbeans.java

--- src/main/java/com/mycompany/proyectonetbeans/ProyectoNetbeans.java
+++ src/main/java/com/mycompany/proyectonetbeans/ProyectoNetbeans.java
@@ -13,5 +13,7 @@ public class ProyectoNetbeans {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         System.out.println("Soy Eduardo");
+        
     }
 }
